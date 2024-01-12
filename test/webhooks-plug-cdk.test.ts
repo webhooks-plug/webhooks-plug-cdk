9aed9226b18f973ca502d810094404da07a11ea6
davetech <davidrums65@gmail.com>
2024-01-13 00:59:46 +0100

    Initial commit

---
+++ test/webhooks-plug-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WebhooksPlugCdk from '../lib/webhooks-plug-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/webhooks-plug-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WebhooksPlugCdk.WebhooksPlugCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
