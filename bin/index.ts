f22f8c90b4fd7e30f1a0f19f7f7ae668aaf58c5a
davetech <davidrums65@gmail.com>
2024-01-31 20:31:19 +0100

    fix(app): correct code structure

--- bin/index.ts
+++ bin/index.ts
@@ -3,28 +3,24 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import LambdaStack from "../lib/lambdas";
-import { Construct } from "constructs";
 import IAMStack from "../lib/iam";
 import { Tags } from "aws-cdk-lib/core";
-
-const app = new cdk.App();
 
 const appName = "WebhooksPlug";
 
 class WebPlugApp extends cdk.App {
   constructor() {
     super();
-    {
-      const lambdaStack = new LambdaStack(this, `${appName}LambdaStack`, {
-        appName,
-      });
-      const iamStack = new IAMStack(this, `${appName}IAMStack`, {
-        appName,
-      });
 
-      Tags.of(lambdaStack).add("Webhooks Infrastructure", "Webplug");
-      Tags.of(iamStack).add("Webhooks Infrastructure", "Webplug");
-    }
+    const lambdaStack = new LambdaStack(this, `${appName}LambdaStack`, {
+      appName,
+    });
+    const iamStack = new IAMStack(this, `${appName}IAMStack`, {
+      appName,
+    });
+
+    Tags.of(lambdaStack).add("Webhooks Infrastructure", "Webplug");
+    Tags.of(iamStack).add("Webhooks Infrastructure", "Webplug");
   }
 }
 
